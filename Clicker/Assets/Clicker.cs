c1a7f0b8d5675e8cdcc0a8b985fe8b6289049b9c
emjerlin <emma.jerlin@elev.ga.ntig.se>
2020-03-12 12:19:20 +0100

    Base Unity

---
+++ Clicker/Assets/Clicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int end = 0;
+
+        int click = 0;
+
+        while (end == 0) { 
+        OnMouseUp();
+        }
+
+
+        int OnMouseUp()
+        {
+            click++;
+
+            return click;
+        }
+
+    }
+}
